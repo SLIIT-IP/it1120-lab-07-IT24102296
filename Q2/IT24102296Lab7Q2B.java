0308571f1585e4945e3a17ee163652ec3eab83db
IT24102296 <it24102296@my.sliit.lk>
2024-09-17 21:36:48 -0700

    IT24102296 Diwanjani B.S.S

---
+++ Q2/IT24102296Lab7Q2B.java
@@ -0,0 +1,16 @@
+public class IT24102296Lab7Q2B
+{
+   public static void main(String[] args)
+   {
+   int i,j;
+   for(i=1; i<=5;i++)
+   {
+      System.out.print(i +"-");
+      for(j=1; j<=5; j++)
+      {
+	System.out.print("*");
+      }
+	System.out.print("\n");
+   }
+   }
+}
